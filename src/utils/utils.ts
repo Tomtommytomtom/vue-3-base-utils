a7138287b3e26383a1a95b365ad3aadf3bfab072
Tom Pillokat <tom.pillokat@googlemail.com>
2020-12-10 20:17:49 +0100

    change debounce implementation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,23 @@
-export function debounce<F extends Function>(func:F, wait:number):F {
-    let timeoutID:number;
-  
-    return <any>function(this:any, ...args: any[]) {
-        clearTimeout(timeoutID);
-        const context = this;
+export const debounce = <F extends Function>(func: F, wait:number, immediate:boolean) => {
+    let timeout: number|null;
 
-        timeoutID = window.setTimeout(function() {
-            func.apply(context, args);
-        }, wait);
-     };
-  };
+    return <any>function executedFunction(this: any) {
+        const context = this;
+        const args = arguments;
+            
+        const later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+        
+        if(timeout){
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(later, wait);
+        
+        if (callNow) func.apply(context, args);
+    };
+};

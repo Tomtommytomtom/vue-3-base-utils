d38b918a4c31a984084f437c24ab33bc3f54d2f7
Tom Pillokat <tom.pillokat@googlemail.com>
2020-12-19 18:42:57 +0100

    Add generic vue composition functions

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { syncPropComputed, syncPropWatch } from "./src/utils/sync"
 export { debounce } from "./src/utils/utils"
 export { AxiosWrapper, TokenAxiosWrapper} from "./src/utils/axios-wrapper"
+export { buildUseFactory } from "./src/utils/vue-axios"
---
+++ src/utils/vue-axios.ts
@@ -0,0 +1,48 @@
+import { AxiosInstance, AxiosResponse } from "axios"
+import { ref, Ref } from "vue"
+
+interface FactoryConfig<T> {
+  instance: AxiosInstance;
+  createItem: () => T;
+  item: T;
+}
+
+interface buildUseGetReturn<T> {
+  fetch: () => Promise<AxiosResponse<T>>;
+  item: Ref<T>
+}
+
+interface buildUsePostReturn<T> {
+  handler: () => Promise<AxiosResponse<T>>
+}
+
+export const buildUseFactory = <T>(config: FactoryConfig<T>) => {
+  const buildUseGet = (path: string): buildUseGetReturn<T> => {
+    const item = ref(config.createItem()) as Ref<T>
+
+    const fetch = () => config.instance
+      .get(path)
+      .then(res => {
+        item.value = res.data
+        return res
+      })
+    
+    return {
+      item,
+      fetch
+    }
+  }
+
+  const buildUsePost = (path: string) => (dataRef: Ref<T>): buildUsePostReturn<T> => {
+    const handler = () => config.instance
+      .post(path,dataRef.value)
+    return {
+      handler
+    }
+  }
+
+  return {
+    buildUseGet,
+    buildUsePost
+  }
+}
